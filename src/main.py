2a5181d82f916896df4c4f110c93f27c004a335f
Yauheni <yazaronak@ibagroup.eu>
2019-10-21 17:12:12 +0300

    Parsing is working. Just printing the list of 30 titles

--- src/main.py
+++ src/main.py
@@ -1,29 +1,20 @@
 import json
 import urllib3.request
 
-
 url = "https://store.playstation.com/valkyrie-api/ru/RU/999/container/STORE-MSF75508-FULLGAMES?sort=release_date&direction=desc&size=30&bucket=games&start=0"
 http = urllib3.PoolManager()
 r = http.request('GET', url)
 print(r.status)
-# print(r.data)
 
 json_string = r.data
-
 data = json.loads(json_string)
 
-# print(type(data))
-# print(data['included'])
-
 full_list = data['included']
-print(type(full_list))
-
 length = len(full_list)
 
-
 for i in range(length):
-    print(i)
     element = full_list[i]
-    type1 = full_list[i]['type']
-    print(type(element))
-    print(type1)
+    element_type = full_list[i]['type']
+    if element_type == 'game':
+        game_name = element['attributes']['name']
+        print(game_name)
