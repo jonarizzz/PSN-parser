6b6eb9c96a3f19bed61a4e63155cbdef9bcf85ca
Yauheni <yazaronak@ibagroup.eu>
2019-11-19 16:35:38 +0300

    Pre-order field found

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import json
 import re
 import urllib3.request
 
-url = "https://store.playstation.com/valkyrie-api/ru/RU/999/container/STORE-MSF75508-FULLGAMES?sort=release_date&direction=desc&size=200&bucket=games&start=0"
+url = "https://store.playstation.com/valkyrie-api/ru/RU/999/container/STORE-MSF75508-FULLGAMES?sort=release_date&direction=desc&size=100&bucket=games&start=0"
 http = urllib3.PoolManager()
 r = http.request('GET', url)
 print(r.status)
@@ -20,14 +20,5 @@ for i in range(len(full_list)):
 
     if element_type == 'game':
         game_name = element['attributes']['name']
-        skip = False
-        for j in range(len(exclude_keywords)):
-            if re.search(exclude_keywords[j-1], game_name):
-                skip = True
-        if not skip:
-            game_titles.append(game_name)
-            # DB storing logic here
 
-x = len(game_titles)
-for b in range(x):
-    print(game_titles[b-1])
+        print(i, game_name, element['attributes']['skus'][0]['is-preorder'])
