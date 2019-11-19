c5868b0e766af10b7949070483904c7c4d7393fb
Yauheni <yazaronak@ibagroup.eu>
2019-11-19 14:13:27 +0300

    Filter for pre orders and digital editions is created. Sorting by regex in th name of the game. To add new word to filter just add it to 'exclude_keywords' list

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 import json
+import re
 import urllib3.request
 
-url = "https://store.playstation.com/valkyrie-api/ru/RU/999/container/STORE-MSF75508-FULLGAMES?sort=release_date&direction=desc&size=30&bucket=games&start=0"
+url = "https://store.playstation.com/valkyrie-api/ru/RU/999/container/STORE-MSF75508-FULLGAMES?sort=release_date&direction=desc&size=200&bucket=games&start=0"
 http = urllib3.PoolManager()
 r = http.request('GET', url)
 print(r.status)
@@ -10,15 +11,23 @@ json_string = r.data
 data = json.loads(json_string)
 
 full_list = data['included']
-length = len(full_list)
+game_titles = list()
+exclude_keywords = ['Цифровое', 'Digital', 'Предзаказ', 'Pre-Order', 'Pre Order', 'предварительный', 'цифрового']
 
-for i in range(length):
+for i in range(len(full_list)):
     element = full_list[i]
     element_type = full_list[i]['type']
+
     if element_type == 'game':
         game_name = element['attributes']['name']
-        long_desc = element['attributes']['long-description']
-        # TODO: sort by popular (genre + popularity mb),
-        #  cut off all pre-orders 
-        print(game_name)
-        print(long_desc)
+        skip = False
+        for j in range(len(exclude_keywords)):
+            if re.search(exclude_keywords[j-1], game_name):
+                skip = True
+        if not skip:
+            game_titles.append(game_name)
+            # DB storing logic here
+
+x = len(game_titles)
+for b in range(x):
+    print(game_titles[b-1])
